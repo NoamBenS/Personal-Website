7d19485337d269af2f79729bdb595587367b05cb
Noam Ben Simon <noampvb@gmail.com>
2024-01-03 14:07:54 -0500

    Changing up home page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,48 @@
 <template>
-  <q-page class="absolute-center" padding>
+  <div class="main" style="display: flex; align-items: center">
     <h3>Home</h3>
-  </q-page>
-  <div
-    class="absolute-center"
-    style="display: flex; flex-direction: table-column; align-items: center"
-  >
     <q-btn color="black" label="Cat Pics" to="/me/catsleep" />
+    <InformationBlock
+      class="box"
+      v-for="block in informationBlocks"
+      :key="block.directory"
+      v-bind="block"
+    />
   </div>
 </template>
 
-<script setup></script>
+<script>
+import InformationBlock from 'components/InformationBlock.vue';
+import { defineComponent } from 'vue';
+
+const blocks = [
+  {
+    image: 'catsleep.jpeg',
+    color: 'orange',
+    label: 'Test1',
+    directory: '/me/catsleep',
+  },
+  {
+    image: 'catsleep.jpeg',
+    color: 'green',
+    label: 'Boy',
+    directory: '/me/catsleep',
+  },
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    InformationBlock,
+  },
+
+  setup() {
+    return {
+      informationBlocks: blocks,
+    };
+  },
+});
+</script>
 
 <style></style>
