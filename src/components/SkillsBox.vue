82e2517eeb47006cf11c741b405c418d01694543
Noam Ben Simon <noampvb@gmail.com>
2024-08-07 13:25:56 -0400

    Updated everything to now include icons !

--- src/components/SkillsBox.vue
+++ src/components/SkillsBox.vue
@@ -6,17 +6,24 @@
           >{{ label }}
         </q-item-label>
       </q-item>
-      <q-item class="skilltext">
-        <p>
-          {{ text }}
-        </p>
+      <q-item class="skillitemsbox">
+        <div v-for="(icon, index) in icons" :key="icon">
+          <img :src="iconImages[index]" alt="icon image" class="iconimage" />
+          <q-tooltip
+            anchor="top middle"
+            self="bottom middle"
+            :offset="[10, 10]"
+          >
+            {{ icon }}
+          </q-tooltip>
+        </div>
       </q-item>
     </div>
   </q-item>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 
 export default defineComponent({
   name: 'SkillsBox',
@@ -25,9 +32,13 @@ export default defineComponent({
       type: String,
       default: '',
     },
-    text: {
-      type: String,
-      default: 'under construction',
+    icons: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
+    iconImages: {
+      type: Array as PropType<string[]>,
+      default: () => [],
     },
     background: {
       type: String,
